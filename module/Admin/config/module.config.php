54a5da31ccc49883b866f0de88ae9e987a69d31a
madhan12 <madhan.tanjung@gmail.com>
2016-05-01 18:14:57 +0700

    Adding Admin Module

---
+++ module/Admin/config/module.config.php
@@ -0,0 +1,58 @@
+<?php
+return array(
+'router' => array(
+        'routes' => array(
+            'admin' => array(
+                'type'    => 'Literal',
+                'options' => array(
+                    'route'    => '/admin',
+                    'defaults' => array(
+                        '__NAMESPACE__' => 'Admin\Controller',
+                        'controller'    => 'Index',
+                        'action'        => 'index',
+                    ),
+                ),
+                'may_terminate' => true,
+                'child_routes' => array(
+                    'default' => array(
+                        'type'    => 'Segment',
+                        'options' => array(
+                            'route'    => '/admin[/:controller[/:action]]',
+                            'constraints' => array(
+                                'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                                'action'     => '[a-zA-Z][a-zA-Z0-9_-]*',
+                            ),
+                            'defaults' => array(
+                            ),
+                        ),
+                    ),
+                ),
+            ),
+        ),
+    ),
+	
+	'controllers' => array(
+        'invokables' => array(
+            'Admin\Controller\Index' => "Admin\Controller\IndexController"
+        ),
+    ),
+    
+	  'view_manager' => array(
+        'display_not_found_reason' => true,
+        'display_exceptions'       => true,
+        'doctype'                  => 'HTML5',
+        'not_found_template'       => 'error/404',
+        'exception_template'       => 'error/index',
+        'template_map' => array(
+        //	'layout/layout'        => __DIR__ . '/../view/layout/layout.phtml',
+           
+        //    'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',
+       //     'kamus/index/index' => __DIR__ . '/../view/kamus/index/login.phtml',
+           
+        ),
+        'template_path_stack' => array(
+            __DIR__ . '/../view',
+        ),
+    ),
+
+);
